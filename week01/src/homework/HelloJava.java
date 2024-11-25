1d2cc3bc215ed1f4fd04c0c157252a25c16a5094
kelly1524 <s2cure24@gmail.com>
2024-11-25 09:19:01 +0900

    241125 1주차 주말 과제

---
+++ week01/src/homework/HelloJava.java
@@ -0,0 +1,13 @@
+package homework; // 패키지명은 homework 입니다
+
+public class HelloJava { // public class 인 HelloJava.java 파일의 시작점입니다.
+	
+	public static void main(String[] args) { // public static void main method 의 시작점입니다.
+		
+		System.out.println("HelloJava");
+//		System 클래스 안의 out 안의 println 이란 method 를 이용하여 
+//		괄호 안의 "HelloJava" 를 출력하고 줄바꿈합니다.
+		
+	} // public static void main method 의 끝입니다.
+
+} // public class 인 HelloJava.java 파일의 끝입니다.
